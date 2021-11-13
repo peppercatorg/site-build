c83e0ee997bac982152622a0f5bf563b71c497b4
Tony Bowden <tony@tmtm.com>
2021-11-13 09:45:06 +0200

    initial setup

---
+++ bin/fetch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+repo=$1; shift
+slug=$1; shift
+name=$*
+
+dir="leaders/$slug"
+csv="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/current.csv"
+html="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/index.html"
+
+echo $name
+mkdir -p $dir
+
+curl -L -o $dir/current.csv $csv
+erb country="$name" csvfile=$dir/current.csv -r csv -T- template/index.erb > $dir/index.html
