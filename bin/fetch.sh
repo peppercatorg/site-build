433f2b3b5a884e545fcb74aa5211de2edfd784b3
Tony Bowden <tony@tmtm.com>
2021-12-16 12:37:53 +0200

    fetch historic leaders+legislators

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -6,10 +6,14 @@ name=$*
 
 dir="docs/leaders/$slug"
 csv="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/current.csv"
+csv21="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/holders21.csv"
+csvmp="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/legislators.csv"
 html="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/index.html"
 
 echo $name
 mkdir -p $dir
 
 curl -L -o $dir/current.csv $csv
+curl -L -o $dir/leaders-historic.csv $csv21
+curl -L -o $dir/legislators-historic.csv $csvmp
 erb country="$name" countrydir=$dir -r csv -T- template/index.erb > $dir/index.html
