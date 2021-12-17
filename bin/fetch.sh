89659869bb7acf9a5cf213641a064640fb918250
Tony Bowden <tony@tmtm.com>
2021-12-17 18:40:28 +0200

    Pass the source URL into the template
    Rather than passing more arguments, switch to just passing a country
    name, and have it look up the data directly.

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 TMPFILE=$(mktemp)
+IFS=$'\n'
 
-repo=$1; shift
-slug=$1; shift
-name=$*
+# This needs to be anchored as some places are full subsets of others,
+# e.g. Ireland / Northern Ireland
+match=($(qsv search -u -i ^$* repos.csv | qsv behead))
+if [[ ${#match[@]} != 1 ]]; then
+  echo "No unique match for $1"
+  printf '\t%s\n' "${match[@]}"
+  exit
+fi
+
+# There's almost certainly a better way to pass these as parameters, but
+# as this is CSV data, which could have spaces and commas in placeames, it's
+# easiest to just extract them one by one.
+name=$(echo ${match[0]} | qsv select 1)
+slug=$(echo ${match[0]} | qsv select 2)
+repo=$(echo ${match[0]} | qsv select 3)
+srce=$(echo ${match[0]} | qsv select 4)
 
 dir="docs/leaders/$slug"
 csv="https://raw.githubusercontent.com/every-politician-scrapers/$repo/main/html/current.csv"
@@ -25,6 +39,6 @@ qsv search -s repo ^$repo repos.csv |
   ifne tee $dir/current.csv
 curl -L -o $dir/leaders-historic.csv $csv21
 curl -L -o $dir/legislators-historic.csv $csvmp
-erb country="$name" countrydir=$dir -r csv -T- template/index.erb > $dir/index.html
+erb country="$name" countrydir=$dir src=$srce -r csv -T- template/index.erb > $dir/index.html
 
 qsv cat rows docs/leaders/**/current.csv > everywhere-current.csv
